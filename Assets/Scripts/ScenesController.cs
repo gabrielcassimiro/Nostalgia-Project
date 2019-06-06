dbf05dc6d53417af9dbeac3b4e62e9bcbc4d1c81
Gabriel Cassimiro <gdinizcassimiro@gmail.com>
2019-06-06 12:32:16 -0300

    Projeto criado e cutscene pronta

---
+++ Assets/Scripts/ScenesController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesController : MonoBehaviour
+{
+    public string MenuName;
+
+    public void goToMenu(){
+        SceneManager.LoadScene(MenuName);
+    }
+}
