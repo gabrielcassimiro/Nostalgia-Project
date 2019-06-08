cfb0080bb09e54a0a335ba681cad2a5b9f91df49
dantasleonardo <47763070+dantasleonardo@users.noreply.github.com>
2019-06-08 14:27:12 -0300

    Mini Game Pong
    Criação do Game Pong

---
+++ Assets/Pong/Scripts/Control.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = .2f;
+
+        if (gameObject.name == "Bump1")
+        {
+            transform.Translate(0, Input.GetAxis("Horizontal") * speed, 0);
+        }
+        else
+            transform.Translate(0, Input.GetAxis("Vertical") * speed, 0);
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -4, 4), 0);
+    }
+}
---
+++ Assets/Pong/Scripts/Points.cs
@@ -0,0 +1,44 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Points : MonoBehaviour
+{
+    public Text points1Text, points2Text;
+
+    private static int points1, points2;
+
+    public GameObject ballPrefab;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<Ball>())
+        {
+            if(gameObject.name == "punctuator1")
+            {
+                points1++;
+                points1Text.text = points1.ToString();
+            }
+            else
+            {
+                points2++;
+                points2Text.text = points2.ToString();
+            }
+            Destroy(other.gameObject);
+            Instantiate(ballPrefab, new Vector3(0, 1, 0), Quaternion.identity);
+        }
+    }
+}
